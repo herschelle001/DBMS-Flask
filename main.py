ca8a4870d6f67a7f6a88b1d43c8eecca6e8f8c7d
ishnai19247 <62013466+ishnai19247@users.noreply.github.com>
2021-04-17 19:19:44 +0300

    query6

--- main.py
+++ main.py
@@ -135,12 +135,12 @@ def execute3( table, value):
         print(i)
 
 def execute4( table, value):
-    print("SELECT * FROM `" + table + " LEFT OUTER JOIN coaching_institute_exam ON"
+    print("SELECT * FROM `" + table + "` LEFT OUTER JOIN coaching_institute_exam ON"
     " (coaching_institute_exam.institute_id= coaching_institute.institute_id) LEFT OUTER JOIN entrance_exams ON"
     " (entrance_exams.exam_id = coaching_institute_exam.exam_id) LEFT OUTER JOIN location ON"
     " (location.location_id = coaching_institute.location_id) "
     " WHERE (`entrance_exams`.`exam_name` = '" + str(value) + "')")
-    cursor.execute("SELECT * FROM `" + table + " LEFT OUTER JOIN coaching_institute_exam ON"
+    cursor.execute("SELECT * FROM `" + table + "` LEFT OUTER JOIN coaching_institute_exam ON"
     " (coaching_institute_exam.institute_id= coaching_institute.institute_id) LEFT OUTER JOIN entrance_exams ON"
     " (entrance_exams.exam_id = coaching_institute_exam.exam_id) LEFT OUTER JOIN location ON"
     " (location.location_id = coaching_institute.location_id) "
@@ -151,13 +151,13 @@ def execute4( table, value):
 
 def execute5( table, value):
     print(
-        "SELECT * FROM `" + table + " LEFT OUTER JOIN coaching_institute_exam ON"
+        "SELECT * FROM `" + table + "` LEFT OUTER JOIN coaching_institute_exam ON"
         " (coaching_institute_exam.institute_id= coaching_institute.institute_id) LEFT OUTER JOIN entrance_exams ON"
         " (entrance_exams.exam_id = coaching_institute_exam.exam_id) LEFT OUTER JOIN location ON"
         " (location.location_id = coaching_institute.location_id) "
         " WHERE (`location`.`state` = '" + str(value) + "')")
     cursor.execute(
-        "SELECT * FROM `" + table + " LEFT OUTER JOIN coaching_institute_exam ON"
+        "SELECT * FROM `" + table + "` LEFT OUTER JOIN coaching_institute_exam ON"
         " (coaching_institute_exam.institute_id= coaching_institute.institute_id) LEFT OUTER JOIN entrance_exams ON"
         " (entrance_exams.exam_id = coaching_institute_exam.exam_id) LEFT OUTER JOIN location ON"
         " (location.location_id = coaching_institute.location_id) "
